8be69b2ecee0bad72e865b6f974251ecc88ade1d
cooltwinpower <49112554+cooltwinpower@users.noreply.github.com>
2019-04-14 12:41:52 -0500

    Add files via upload
    DB: Mysql
    Connection info: config.py

--- mini_tweeter.py
+++ mini_tweeter.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, current_app
 from flask.json import JSONEncoder
+from sqlalchemy import create_engine, text
 
 
 class CustomJSONEncoder(JSONEncoder):
@@ -12,110 +13,181 @@ class CustomJSONEncoder(JSONEncoder):
         return JSONEncoder.default(self, o)
 
 
-app = Flask(__name__)
-app.users = {}
-app.id_count = 1
-app.tweets = []
+def get_user(user_id):
+    # Find user from DB
+    # Input: user_id
+    # Return: User information in JSON format
+    user = current_app.database.execute(text("""
+        SELECT
+            id,
+            name,
+            email,
+            profile
+        FROM users
+        WHERE id = :user_id
+    """), {
+        'user_id' : user_id
+    }).fetchone()
 
-# assign Custom JSON encoder as default in this program
-app.json_encoder = CustomJSONEncoder
-
-
-@app.route("/Pingapi", methods=['GET'])
-# Verification endpoint whether server is alive or not
-# If server works, it'll return 'pong'
-def ping():
-    return 'pong'
-
-
-@app.route("/signup", methods=['POST'])
-# sign up endpoint. Register user, then return user information with user_ID
-def sign_up():
-    new_user = request.json
-    new_user["id"] = app.id_count
-    app.users[app.id_count] = new_user
-    app.id_count = app.id_count + 1
-
-    return jsonify(new_user)
+    return {
+        'id' : user['id'],
+        'name' : user['name'],
+        'email' : user['email'],
+        'profile' : user['profile']
+    } if user else None
 
 
-@app.route("/tweet", methods=['POST'])
-# tweet post endpoint. post tweet under user account
-# character limit per tweet is 300
-def tweet():
-    payload = request.json
-    user_id = int(payload['id'])
-    tweet = payload["tweet"]
-
-    if user_id not in app.users:
-        return 'Invalis user id', 400
-
-    if len(tweet) > 300:
-        return 'No more than 300 characters!', 400
-
-    user_id = int(payload['id'])
-
-    app.tweets.append({
-        'user_id' : user_id,
-        'tweet' : tweet
-    })
-
-    return '', 200
+def insert_user(user):
+    # Register new user in DB
+    # Input: user info
+    return current_app.database.execute(text("""
+        INSERT INTO users (
+            name,
+            email,
+            profile,
+            hashed_password
+        ) VALUES (
+            :name,
+            :email,
+            :profile,
+            :password
+        )
+    """), user).lastrowid
 
 
-@app.route("/follow", methods=['POST'])
-# Follow endpoint
-# if user_id does not have follow field, empty field will be created automatically
-# return user information with following status
-def follow():
-    payload = request.json
-    user_id = int(payload['id'])
-    follow = int(payload['follow'])
-
-    if user_id not in app.users:
-        return 'Invalid User ID', 400
-
-    if follow not in app.users:
-        return 'Invalid follower ID', 400
-
-    user = app.users[user_id]
-    user.setdefault('follow', set()).add(follow)
-
-    return jsonify(user)
+def insert_tweet(user_tweet):
+    # Post User tweet in DB
+    # Input: user's tweet
+    return current_app.database.execute(text("""
+        INSERT INTO tweets (
+            user_id,
+            tweet
+        ) VALUES (
+            :id,
+            :tweet
+        )
+    """), user_tweet).rowcount
 
 
-@app.route("/unfollow", methods=['POST'])
-# Unfollow endpoint
-# return user information with following status
-def un_follow():
-    payload = request.json
-    user_id = int(payload['id'])
-    un_follow = int(payload['un_follow'])
-
-    if user_id not in app.users:
-        return 'Invalid User ID', 400
-
-    if un_follow not in app.users:
-        return 'Invalid follower ID', 400
-
-    user = app.users[user_id]
-    user.setdefault('follow', set()).discard(un_follow)
-
-    return jsonify(user)
+def insert_follow(user_follow):
+    # Register follower to user follower list in DB
+    # Input: Follow ID
+    return current_app.database.execute(text("""
+        INSERT INTO users_follow_list (
+            user_id,
+            follow_user_id
+        ) VALUES (
+            :id,
+            :follow
+        )
+    """), user_follow).rowcount
 
 
-@app.route("/time_line/<int:user_id>", methods=['GET'])
-# timeline endpoint.
-# return follower's post
-def timeline(user_id):
-    if user_id not in app.users:
-        return 'Invalid User id', 400
+def insert_unfollow(user_unfollow):
+    # Remove unfollow user ID from user follow list in DB
+    # Input: Unfollow user ID
+    return current_app.database.execute(text("""
+        DELETE FROM users_follow_list
+        WHERE user_id = :id
+        AND follow_user_id = :unfollow
+    """), user_unfollow).rowcount
 
-    follow_list = app.users[user_id].get('follow', set())
-    follow_list.add(user_id)
-    timeline = [tweet for tweet in app.tweets if tweet['user_id'] in follow_list]
 
-    return jsonify({
-        'user_id' : user_id,
-        'timeline' : timeline
-    })
+def get_timeline(user_id):
+    # Show all follower's tweets
+    # Input: User ID
+    timeline = current_app.database.execute(text("""
+        SELECT 
+            t.user_id,
+            t.tweet
+        FROM tweets t
+        LEFT JOIN user_follow_list ufl ON ufl.user_id = :user_id
+        WHERE t.user_id = :user_id
+        OR t.user_id = ufl.follow_user_id
+    """), {
+        'user_id' : user_id
+    }).fetchall()
+
+    return [{
+        'user_id' : tweet['user_id'],
+        'tweet' : tweet['tweet']
+    } for tweet in timeline]
+
+
+def create_app(test_config=None):
+    # Creating Endpoint for Front end
+    # Setup DB information through config.py or parameter which is test_config
+    # Config for DB connection
+    app = Flask(__name__)
+
+    # assign Custom JSON encoder as default in this program
+    app.json_encoder = CustomJSONEncoder
+
+    if test_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.update(test_config)
+
+    database = create_engine(app.config['DB_URL'], encoding = 'utf-8', max_overflow = 0)
+    app.database = database
+
+    @app.route("/Pingapi", methods=['GET'])
+    # Verification endpoint whether server is alive or not
+    # If server works, it'll return 'pong'
+    def ping():
+        return 'pong'
+
+    @app.route("/signup", methods=['POST'])
+    # sign up endpoint. Register user, then return user information with user_ID
+    def sign_up():
+        new_user = request.json
+        new_user_id = insert_user(new_user)
+        new_user = get_user(new_user_id)
+
+        return jsonify(new_user)
+
+    @app.route("/tweet", methods=['POST'])
+    # tweet post endpoint. post tweet under user account
+    # character limit per tweet is 300
+    def tweet():
+        user_tweet = request.json
+        tweet = user_tweet['tweet']
+
+        if len(tweet) > 300:
+            return 'No more than 300 characters!', 400
+
+        insert_tweet(user_tweet)
+
+        return '', 200
+
+    @app.route("/follow", methods=['POST'])
+    # Follow endpoint
+    # if user_id does not have follow field, empty field will be created automatically
+    # return user information with following status
+    def follow():
+        payload = request.json
+        insert_follow(payload)
+
+        return '', 200
+
+    @app.route("/unfollow", methods=['POST'])
+    # Unfollow endpoint
+    # return user information with following status
+    def un_follow():
+        payload = request.json
+        insert_unfollow(payload)
+
+        return '', 200
+
+    @app.route("/time_line/<int:user_id>", methods=['GET'])
+    # timeline endpoint.
+    # return follower's post
+    def timeline(user_id):
+        return jsonify({
+            'user_id': user_id,
+            'timeline': get_timeline(user_id)
+        })
+
+    return app
+
+
